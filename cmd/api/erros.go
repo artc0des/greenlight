4bbd2d4559dfc146ec8f3f2a2c64e14a136c35be
artc0des <art.c@hey.com>
2025-04-04 23:56:09 -0500

    more auth

--- cmd/api/erros.go
+++ cmd/api/erros.go
@@ -56,3 +56,7 @@ func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http
 func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
 	app.errorResponse(w, r, http.StatusTooManyRequests, "invalid authentication credentials")
 }
+
+func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	app.errorResponse(w, r, http.StatusUnauthorized, "invalid or missing authentication token")
+}
